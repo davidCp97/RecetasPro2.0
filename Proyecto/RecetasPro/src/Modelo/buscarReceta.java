37e51b32ae6badbfccfee79848ef634b8005c9df
SALA303-055\usuario <gutjei95@gmail.com>
2017-03-01 15:26:41 -0500

    Se creó la clase buscarReceta la cual se encarga de recibir un nombre de receta y desplegar en pantalla toda la información detallada de la misma.

---
+++ Proyecto/RecetasPro/src/Modelo/buscarReceta.java
@@ -0,0 +1,76 @@
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class buscarReceta {
+   
+    static Connection con;
+    static Statement st;
+    static PreparedStatement pst;
+    static PreparedStatement pst2;
+    static ResultSet rs;
+    static ResultSet rs2;
+
+public static void main(String[] args)throws SQLException{
+
+         
+          System.out.println(mostrarReceta("Arroz Hindí"));
+
+    }
+    
+   //Método que busca el nombre de la receta y muestra la información de la misma receta
+    public static String mostrarReceta(String q){
+         ArrayList<String> ar = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        String m="";
+          try{
+
+            String dbURL = "jdbc:ucanaccess://E:\\Nueva carpeta/Rest1.accdb";//Dorección base de datos
+            String username = "";
+            String password = "";
+            String nom="";
+            
+            con = DriverManager.getConnection(dbURL, username, password);
+            String query = "SELECT codigo_receta, nombre, descripcion, tiempo  FROM Receta WHERE  nombre like \""+q+"*\";";//COnsulta
+            
+            pst = con.prepareStatement(query);
+            rs = null;
+            rs2 = null;
+         
+            try{
+                rs = pst.executeQuery();
+                
+                while(rs.next())
+                {
+                 //Se guardan las variables en un String
+                    m+=rs.getString("codigo_receta")+"\n";
+                    m+=rs.getString("nombre")+"\n";
+                    m+=rs.getString("descripcion")+"\n";
+                    m+=rs.getString("tiempo")+"\n";
+                    
+                }
+ 
+                 
+               
+            }catch(SQLException sql){}
+            pst.close();
+        
+            con.close();
+             
+        }
+        catch(SQLException e)
+        {}
+          return m;
+    }
+  
+}
