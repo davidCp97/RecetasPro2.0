f3f9cdeefbad7cc2be580f55529e91ed4203b82d
Cristian López <cristianc.lopezr@konradlorenz.edu.co>
2017-03-01 15:54:29 -0500

    Se creo una nueva clase llama CrearReceta, en esta clase se crearon los metodos ingresotabla y mostraringredientes, el primer metodo ingresara a la base de datos el nombre de la receta, la descripcion, el iempo y sus ingrediente, mientras que el segundo metodo mostrar los ingredientes que ha ingresado el usuario a esa receta, tambien se modifico la clase conexion con lo smetodos ultimoid y consulta ingrediente, el primer metodo me trae el ultimo id de la tabla receta y el segundo metodo consulta los ingredientes de la tabla receta

--- Proyecto/RecetasPro/src/Modelo/Conexion.java
+++ Proyecto/RecetasPro/src/Modelo/Conexion.java
@@ -9,8 +9,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
@@ -23,13 +26,10 @@ public class Conexion {
     static PreparedStatement pst;
     static ResultSet rs;
 
-    
-    
-    
-    
+    //Método base para realizar una consulta a la Base de Datos
     public void consultaABaseDeDatosBase(String consulta) {
         try {
-            String dbURL = "jdbc:ucanaccess://C:\\Users\\Ingenieria\\Downloads\\Pre\\Nueva carpeta/Rest1.accdb"; //Dirección de donde está la base de datos
+            String dbURL = "jdbc:ucanaccess://C:\\Users\\usuario\\Downloads\\Versiones RecetasPro\\Base de datos RecetasPro/Recetas.accdb"; //Dirección de donde está la base de datos
             String username = ""; //Usuario de la base de datos
             String password = ""; //Password del usuario de la base de datos
             con = DriverManager.getConnection(dbURL, username, password);
@@ -56,9 +56,10 @@ public class Conexion {
         }
     }
 
+    //Método base para realizar una inserción en la base de datos
     public void insertarABaseDeDatosBase(String insercion) {
         try {
-            String dbURL = "jdbc:ucanaccess://C:\\Users\\Ingenieria\\Downloads\\Pre\\Nueva carpeta\\Rest1.accdb"; //Dirección de donde está la base de datos
+            String dbURL = "jdbc:ucanaccess://C:\\Users\\usuario\\Downloads\\Versiones RecetasPro\\Base de datos RecetasPro/Recetas.accdb"; //Dirección de donde está la base de datos
             String username = ""; //Usuario de la base de datos
             String password = ""; //Password del usuario de la base de datos
             con = DriverManager.getConnection(dbURL, username, password);
@@ -73,6 +74,81 @@ public class Conexion {
             con.close();
         } catch (SQLException e) {
         }
+    }
+
+    //Método que regresa un entero, el cual es el código (Primary Key) de la última Receta
+    public int consultaUltimoID() {
+        int a = 0;
+        try {
+            String dbURL = "jdbc:ucanaccess://C:\\Users\\usuario\\Downloads\\Versiones RecetasPro\\Base de datos RecetasPro/Recetas.accdb"; //Dirección de donde está la base de datos
+            String username = ""; //Usuario de la base de datos
+            String password = ""; //Password del usuario de la base de datos
+
+            con = DriverManager.getConnection(dbURL, username, password);
+            String query = "SELECT TOP 1 * FROM Receta ORDER BY codigo_receta DESC "; //query que solicita la última fila de datos
+            pst = con.prepareStatement(query);
+            rs = null;
+            try {
+                rs = pst.executeQuery();
+                while (rs.next()) {
+                    a = rs.getInt("codigo_receta"); //Se pide que solo regrese el último código de la receta.
+                }
+            } catch (SQLException sql) {
+            }
+            pst.close();
+            con.close();
+        } catch (SQLException e) {
+        }
+        return a;
+    }
+
+    //Método que devuelve en un arraylist la lista de los ingredientes totales.
+    public ArrayList consultaIngredientes() {
+        ArrayList result = new ArrayList<>();
+        try {
+           String dbURL = "jdbc:ucanaccess://C:\\Users\\usuario\\Downloads\\Versiones RecetasPro\\Base de datos RecetasPro/Recetas.accdb"; //Dirección de donde está la base de datos
+            String username = ""; //Usuario de la base de datos
+            String password = ""; //Password del usuario de la base de datos
+            con = DriverManager.getConnection(dbURL, username, password);
+            String query = "Select * from Ingrediente"; //query que pide traer los datos de la tabla Ingrediente.
+            pst = con.prepareStatement(query);
+            rs = null;
+            try {
+                rs = pst.executeQuery();
+                while (rs.next()) {
+                    result.add(rs.getString("nombre_ingrediente"));//Se solicita únicamente la columna de Nombre_ingrediente
+                }
+            } catch (SQLException sql) {
+            }
+            pst.close();
+            con.close();
+        } catch (SQLException e) {
+        }
+        return result;
+    }
+    
+    public ArrayList consultaCodigosRecetas() {
+        ArrayList result = new ArrayList<>();
+        try {
+           String dbURL = "jdbc:ucanaccess://C:\\Users\\usuario\\Downloads\\Versiones RecetasPro\\Base de datos RecetasPro/Recetas.accdb"; //Dirección de donde está la base de datos
+            String username = ""; //Usuario de la base de datos
+            String password = ""; //Password del usuario de la base de datos
+            con = DriverManager.getConnection(dbURL, username, password);
+            String query = "select codigo_receta from Receta;"; 
+            pst = con.prepareStatement(query);
+            rs = null;
+            try {
+                rs = pst.executeQuery();
+                while (rs.next()) {
+                    result.add(rs.getString("codigo_receta"));
+                }
+            } catch (SQLException sql) {
+            }
+            pst.close();
+            con.close();
+        } catch (SQLException e) {
+        }
+        return result;
     }
 
 }
